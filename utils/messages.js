85b56af70fefff162aad687c754ae860b8055a5f
leins275 <leins275@gmail.com>
2020-11-14 16:40:21 +0300

    Added new error messages

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,8 @@
 const employeeMsg = {
     BAD_ID: id => `Employee id should be a integer! given: ${id}`,
     NOT_FOUND: id => `Employee with id=${id} not found`,
-    NO_COLLECTION: 'Employees collection not found in database'
+    NO_COLLECTION: 'Employees collection not found in database',
+    DB_ID_ERR: "Can not get new user id from db"
 };
 
 const routerMsg = {
