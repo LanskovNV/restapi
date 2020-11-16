7fad957a43774927135d8d4d36ce759757e0662b
leins275 <leins275@gmail.com>
2020-11-17 00:45:08 +0300

    Added SwaggerUi support with default config

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const Boom = require('boom');
 const { routerMsg } = require('../utils/messages');
 const employeeRoutes = require('./employee-routes');
 const authRoutes = require('./auth-routes');
+const swaggerRoutes = require('./swagger');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/employees', employeeRoutes);
+router.use('/swagger', swaggerRoutes);
 router.all('*', (req, res) => {
     res.json(Boom.notFound(routerMsg.BAD_REQ));
 });
