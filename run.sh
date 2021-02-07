4abb8c98bc1e5b5f2e9941a464329c4033430921
leins275 <leins275@localhost.localdomain>
2021-02-07 12:17:02 +0300

    Added run.sh to deploy

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name=crud_api
+
+docker stop $name || true
+docker rm $name || true
+docker build -t $name .
+docker run -d \
+    --restart=unless-stopped \
+    --name $name \
+    -p 5000:5000 \
+    $name
