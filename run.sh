c0ba7d7b31da31d203d414a0bb8b082a02cafc43
LanskovNV <leins275@gmail.com>
2021-03-06 13:50:17 +0300

    Updated

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 echo "AUTH_SECRET=123 HOST=0.0.0.0 PORT=5000 PAGE_SIZE=5 DB_URL=mongodb://api_db:27017/employeeDb" | tr " " "\n" > .env
 
 docker-compose down
+mkdir -p data
+chown -R $USER: data
 docker-compose up -d --build
