992cd50d0373e966a74df00a95629fd5dafd379d
LanskovNV <leins275@gmail.com>
2021-03-06 13:20:00 +0300

    Updated

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 echo "AUTH_SECRET=123 HOST=0.0.0.0 PORT=5000 PAGE_SIZE=5 DB_URL=mongodb://api_db:27017/employeeDb" | tr " " "\n" > .env
 
-git pull
 docker-compose rm -fs
 docker-compose up -d --build
