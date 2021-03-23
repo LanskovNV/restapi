c6d5730e4e4c55fd8342b3272807db7d09cec810
LanskovNV <leins275@gmail.com>
2021-03-24 01:31:21 +0300

    Added cors support

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const cors = require('cors');
 const { Strategy } = require('passport-http-bearer');
 const routes = require('../routes');
 const { verify } = require('./jwt-token');
@@ -8,6 +9,7 @@ passport.use('bearerAuth', new Strategy(verify));
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize(undefined));
