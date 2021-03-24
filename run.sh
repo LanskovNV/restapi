f2e6d950a30e053dabf708b13bfb77225ed77fb6
LanskovNV <leins275@gmail.com>
2021-03-24 05:35:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 echo "AUTH_SECRET=123 HOST=0.0.0.0 PORT=5000 PAGE_SIZE=5 DB_URL=mongodb://api_db:27017/employeeDb" | tr " " "\n" > .env
 
 docker-compose down
-mkdir -p data
-chown -R $USER: data
+mkdir -p /home/leins275/volumens/restapi
+# chown -R leins: /home/leins275/volumens/restapi
 docker-compose up -d --build
